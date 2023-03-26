96692dc57206de52a032b46621cd4a5222a23232
Илья Рыжков <ryzhkov.ilya17@gmail.com>
2023-03-26 21:31:53 +0400

    C argument was added

--- LinearRegressions/linear_regression.py
+++ LinearRegressions/linear_regression.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 class LinearRegressionGD:
     """Linear Regression Using Gradient Descent.
         Also Ridge, Lasso and Elastic Net regression
@@ -14,12 +13,12 @@ class LinearRegressionGD:
     """
 
     def __init__(self,
-                method:str='linear'):
-        method_list = ['linear', 'lasso', 'ridge', 'elastic']
+                penalty:str=None):
+        method_list = ['l1', 'l2', 'elasticnet', None]
         assert \
-            method.lower() in method_list, \
-            f'This method not found. Please give one of this method {method_list}. Receive method = {method.lower()}'
-        self.__method = method
+            penalty.lower() in method_list, \
+            f'This method not found. Please give one of this method {method_list}. Receive method = {penalty.lower()}'
+        self.__penalty = penalty
 
     def __check_params(self):
         """Check input params
@@ -31,30 +30,29 @@ class LinearRegressionGD:
             isinstance(self.__n_iterations, int),\
             f'N_iterations must be only integer. Receive {type(self.__n_iterations)}.'
         assert\
-            isinstance(self.__l2_penalty, float)|isinstance(self.__l2_penalty, int),\
-            f'L2_penalty must be only integer or float. Receive {type(self.__l2_penalty)}.'
-        assert\
-            isinstance(self.__l1_penalty, float)|isinstance(self.__l1_penalty, int),\
-            f'L1_penalty must be only integer or float. Receive {type(self.__l1_penalty)}.'
+            isinstance(self.__C, float)|isinstance(self.__C, int),\
+            f'C must be only integer or float. Receive {type(self.__C)}.'
         assert\
             (self.__X.shape[0]>0)&(self.__X.shape[1]>0),\
             f'X must not be empty.'
         assert\
             (self.__y.shape[0]==self.__X.shape[0]),\
             f'Y shape must be equal X shape.'
-        if self.__method == 'elastic':
-            assert\
-            (self.__l1_penalty is not None)&(self.__l2_penalty is not None)
+
 
     def __calculate_gradient(self):
-        if self.__method.lower() == 'ridge':
-            # If we have a ridge regression we find the gradient
-            self.__dW = ((-2*np.dot(self.__X.T, self.__residuals))+\
-                            (2*self.__l2_penalty*self.coef_))/self.__m
-        elif self.__method.lower() == 'linear':
+        """Function for find out weight and b gradients
+        """
+        if self.__penalty.lower() == None:
             # If the usual linear regression we find the gradient
             self.__dW = (-2*np.dot(self.__X.T, self.__residuals))/self.__m
-        elif self.__method.lower() == 'lasso':
+
+        elif self.__penalty.lower() == 'l2':
+            # If we have a ridge regression we find the gradient
+            self.__dW = ((-2*np.dot(self.__X.T, self.__residuals))+\
+                            (2*self.__C*self.coef_))/self.__m
+            
+        elif self.__penalty.lower() == 'l1':
             # If we have a lasso regression we find the gradient
             # Create a gradient matrix
             self.__dW = np.zeros(self.n_features_in_)
@@ -62,10 +60,11 @@ class LinearRegressionGD:
             for j in range(self.n_features_in_) :
                 if self.coef_[j]>0 :
                     self.__dW[j]=((-2*np.dot(self.__X.T, self.__residuals))+\
-                                    (self.__l1_penalty*self.coef_[j]))/self.__m
+                                    (self.__C*self.coef_[j]))/self.__m
                 else :
                     self.__dW[j]=((-2*np.dot(self.__X.T, self.__residuals))-\
-                                    (self.__l1_penalty*self.coef_[j]))/self.__m
+                                    (self.__C*self.coef_[j]))/self.__m
+                    
         else:
             # If we have elastic regression find the gradient
             # Create a gradient matrix
@@ -74,17 +73,18 @@ class LinearRegressionGD:
             for j in range(self.n_features_in_) :
                 if self.coef_[j]>0 :
                     self.__dW[j]=((-2*np.dot(self.__X.T, self.__residuals))+\
-                                    (self.__l1_penalty*self.coef_[j])+\
-                                    (2*self.__l2_penalty*self.coef_))/self.__m
+                                    (self.__C*self.coef_[j])+\
+                                    (2*self.__C*self.coef_))/self.__m
                 else :
                     self.__dW[j]=((-2*np.dot(self.__X.T, self.__residuals))-\
-                                    (self.__l1_penalty*self.coef_[j])+\
-                                    (2*self.__l2_penalty*self.coef_))/self.__m
+                                    (self.__C*self.coef_[j])+\
+                                    (2*self.__C*self.coef_))/self.__m
         # Find the gradient for the free term b
         self.__db = - 2 * np.sum(self.__residuals) / self.__m 
     
     def __update_weights(self):
-        
+        """Update weights and b
+        """
         y_pred = self.predict(self.__X)
         # Deviation from the true value
         self.__residuals = self.__y - y_pred
@@ -98,10 +98,9 @@ class LinearRegressionGD:
     def fit(self, 
             x, 
             y,
-            learning_rate:float=None,
-            n_iterations:int=None,
-            l2_penalty:float=None,
-            l1_penalty:float=None):
+            learning_rate:float=0.001,
+            C:float=1.0,
+            n_iterations:int=1000):
         """Fit the training data
         Parameters
         ----------
@@ -110,9 +109,8 @@ class LinearRegressionGD:
         y : array-like, shape = [n_samples, n_target_values]
             Target values
         learning_rate: float, learning rate coeff
+        C: float , Inverse of regularization strength; must be a positive float. Like in support vector machines, smaller values specify stronger regularization.
         n_iterations: int, count of inerations
-        l2_penalty: float, penalty coeff if method = ridge or elastic
-        l1_penalty: float, penalty coeff if method = lasso or elastic
         Returns
         -------
         self : object
@@ -121,8 +119,7 @@ class LinearRegressionGD:
         self.__y = y
         self.__learning_rate = learning_rate if learning_rate is not None else 0.001
         self.__n_iterations = n_iterations if n_iterations is not None else 1000
-        self.__l2_penalty = l2_penalty if l2_penalty is not None and self.__method in ['ridge', 'elastic'] else None
-        self.__l1_penalty = l1_penalty if l1_penalty is not None and self.__method in ['lasso', 'elastic'] else None
+        self.__C = C
 
         # Check correct params
         self.__check_params()
